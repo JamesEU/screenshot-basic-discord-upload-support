b2b5d383cf9fd2493cdd4f72fa2c3552cc9a0ef6
JamesUK2020 <60899490+JamesUK2020@users.noreply.github.com>
2020-10-22 11:32:46 +0100

    Fix resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,6 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_script 'dist/client.js'
 server_script 'dist/server.js'
+server_script 'uploadfile.js'
 
 dependency 'yarn'
 dependency 'webpack'
